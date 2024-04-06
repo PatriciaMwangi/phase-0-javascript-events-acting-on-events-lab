22218c0f510b5a362eea16a9778928157581e23a
patricia <patricia.mwangi@student.moringaschool.com>
2024-04-06 16:21:27 +0300

    dodger

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function(e){
+    if(e.key==="ArrowLeft"){
+        moveDodgerLeft()
+    }
+  })
+  function moveDodgerRight(){
+    let dodger=document.getElementById("dodger")
+    let newPosition = parseInt(dodger.style.left.replace("px",''));
+    if(newPosition<360){
+      dodger.style.left=`${newPosition+1}px`;
+    }
+  }
+  document.addEventListener("keydown",function(e){
+    if(e.key==="ArrowRight"){
+      moveDodgerRight()
+    }
+  })
